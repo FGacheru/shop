bdfbcb1816116190fe77d1213f18fac8753e1879
FGacheru <francisgacheru2001@gmail.com>
2021-01-29 04:17:57 +0300

    create 3 views to render the templates in all-store

--- eDuka/views.py
+++ eDuka/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'all-store/index.html', context)
+
+def CommonPasswordValidator(request):
+    context = {}
+    return render(request, 'all-store/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'all-store/checkout.html', context)
